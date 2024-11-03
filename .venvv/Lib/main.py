c282ec6464494d22555dd9c6e5e68b4309cf1968
Nikint86 <lobackiy_2007@mail.ru>
2024-11-03 13:31:01 +0300

    код

--- .venvv/Lib/main.py
+++ .venvv/Lib/main.py
@@ -1,19 +1,13 @@
 import json
-
-
-from itertools import product
-
+import os
 
 import requests
-
-
-import geopy
 
 
 from geopy import distance
 
 
-from pprint import pprint
+from dotenv import load_dotenv
 
 
 import folium
@@ -21,9 +15,8 @@ import folium
 
 from flask import Flask
 
-if __name__ == '__main__':
 
- def fetch_coordinates(apikey, address):
+def fetch_coordinates(apikey, address):
     base_url = 'https://geocode-maps.yandex.ru/1.x'
     response = requests.get(base_url, params={
         'geocode': address,
@@ -41,16 +34,19 @@ if __name__ == '__main__':
     return lon, lat
 
 
+def get_func_distanse(cofies):
+    return  cofies['Distanse']
+
+
 my_file = open('coffee.json', 'r', encoding='CP1251')
 file_contents = my_file.read()
 cordinates = json.loads(file_contents)
 my_file.close()
 all_cofeina = cordinates
-apikey = '17d42f3a-40e2-46e6-a82f-314e0bbbe9ed'
-print('Где Вы находитесь?\n')
+load_dotenv()
+apikey = os.getenv('apikey')
 inputcord = input()
 coords = fetch_coordinates(apikey, inputcord)
-print('Ваши координаты:',coords)
 
 cofe_spisok = list()
 
@@ -59,66 +55,38 @@ for all_cofeina in cordinates:
     all_cofeina_longitude = all_cofeina['Longitude_WGS84']
     all_cofeina_latitude = all_cofeina['Latitude_WGS84']
     all_cofeina_coordinates = [all_cofeina_longitude,all_cofeina_latitude]
+    all_cofeina_coordinates_two = [all_cofeina_latitude,all_cofeina_longitude]
     func_distanse = (distance.distance(coords, all_cofeina_coordinates).km)
     cofee = dict()
     cofee['Name'] = all_cofeina_name
-    cofee['Coordinates'] = all_cofeina_coordinates
+    cofee['Coordinates'] = all_cofeina_coordinates_two
     cofee['Distanse'] = func_distanse
     cofe_spisok.append(cofee)
 
-def get_func_distanse(cofies):
-    return  cofies['Distanse']
-
 min_distanse = sorted(cofe_spisok, key = get_func_distanse)
 first_page = min_distanse[:5]
-first_cofe_marker = (first_page[0]['Coordinates'])
-first_cofe_name = (first_page[0]['Name'])
-two_cofe_marker = (first_page[1]['Coordinates'])
-two_cofe_name = (first_page[1]['Name'])
-three_cofe_marker = (first_page[2]['Coordinates'])
-three_cofe_name = (first_page[2]['Name'])
-four_cofe_marker = (first_page[3]['Coordinates'])
-four_cofe_name = (first_page[3]['Name'])
-five_cofe_marker = (first_page[4]['Coordinates'])
-five_cofe_name = (first_page[4]['Name'])
-
 m = folium.Map(location=(coords[1],coords[0]),zoom_start=17)
+first_cofe_marker = list()
+first_cofe_name = list()
+first_cofe_marker = first_page[1]
+print(first_cofe_marker['Coordinates'])
+first_cofe_name = first_page[1]
+print(first_cofe_name['Name'])
+
+for first_cofe_marker in first_page:
+    folium.Marker(
+    location= first_cofe_marker['Coordinates'],
+    tooltip='Нажми на меня!',
+    popup=first_cofe_name['Name'],
+    icon=folium.Icon(icon='home'),
+    ).add_to(m)
 
 folium.Marker(
-    location=[first_cofe_marker[1],first_cofe_marker[0]],
-    tooltip='Нажми на меня!',
-    popup=first_cofe_name,
-    icon=folium.Icon(icon='cloud'),
-).add_to(m)
-
-folium.Marker(
-    location=[two_cofe_marker[1],two_cofe_marker[0]],
-    tooltip='Нажми на меня!',
-    popup=two_cofe_name,
-    icon=folium.Icon(icon='cloud'),
-).add_to(m)
-
-folium.Marker(
-    location=[three_cofe_marker[1],three_cofe_marker[0]],
-    tooltip='Нажми на меня!',
-    popup=three_cofe_name,
-    icon=folium.Icon(icon='cloud'),
-).add_to(m)
-
-folium.Marker(
-    location=[four_cofe_marker[1],four_cofe_marker[0]],
-    tooltip='Нажми на меня!',
-    popup=four_cofe_name,
-    icon=folium.Icon(icon='cloud'),
-).add_to(m)
-
-folium.Marker(
-    location=[five_cofe_marker[1],five_cofe_marker[0]],
-    tooltip='Нажми на меня!',
-    popup=five_cofe_name,
-    icon=folium.Icon(icon='cloud'),
-).add_to(m)
-
+    location= (coords[1],coords[0]),
+    tooltip='Вы тут',
+    popup='Вы тут',
+    icon=folium.Icon(icon='none'),
+    ).add_to(m)
 m.save('map.html')
 
 def hello_world():
@@ -128,3 +96,5 @@ def hello_world():
 app = Flask(__name__)
 app.add_url_rule('/', 'hello', hello_world)
 app.run('0.0.0.0')
+if __name__ == '__main__':
+    main()
