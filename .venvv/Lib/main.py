68eeff5a2e880db9fb8d84d1ad39bdfb17f6c576
Nikint86 <lobackiy_2007@mail.ru>
2024-11-05 12:24:49 +0300

    код

--- .venvv/Lib/main.py
+++ .venvv/Lib/main.py
@@ -1,18 +1,9 @@
 import json
 import os
-
 import requests
-
-
 from geopy import distance
-
-
 from dotenv import load_dotenv
-
-
 import folium
-
-
 from flask import Flask
 
 
@@ -35,66 +26,61 @@ def fetch_coordinates(apikey, address):
 
 
 def get_func_distanse(cofies):
-    return  cofies['Distanse']
+    return cofies['Distanse']
 
 
 def hello_world():
     with open('map.html') as file:
-      return file.read()
+        return file.read()
 
-
-my_file = open('coffee.json', 'r', encoding='CP1251')
-file_contents = my_file.read()
-cordinates = json.loads(file_contents)
-my_file.close()
-all_cofeina = cordinates
-load_dotenv()
-apikey = os.getenv('apikey')
-inputcord = input()
-coords = fetch_coordinates(apikey, inputcord)
-
-cofe_spisok = list()
-
-for all_cofeina in cordinates:
-    all_cofeina_name =  all_cofeina['Name']
-    all_cofeina_longitude = all_cofeina['Longitude_WGS84']
-    all_cofeina_latitude = all_cofeina['Latitude_WGS84']
-    all_cofeina_coordinates = [all_cofeina_longitude,all_cofeina_latitude]
-    all_cofeina_coordinates_two = [all_cofeina_latitude,all_cofeina_longitude]
-    func_distanse = (distance.distance(coords, all_cofeina_coordinates).km)
-    cofee = dict()
-    cofee['Name'] = all_cofeina_name
-    cofee['Coordinates'] = all_cofeina_coordinates_two
-    cofee['Distanse'] = func_distanse
-    cofe_spisok.append(cofee)
-
-min_distanse = sorted(cofe_spisok, key = get_func_distanse)
-first_page = min_distanse[:5]
-m = folium.Map(location=(coords[1],coords[0]),zoom_start=17)
-first_cofe_marker = list()
-first_cofe_name = list()
-first_cofe_marker = first_page[1]
-first_cofe_name = first_page[1]
-
-for first_cofe_marker in first_page:
-    folium.Marker(
-    location= first_cofe_marker['Coordinates'],
-    tooltip='Нажми на меня!',
-    popup=first_cofe_name['Name'],
-    icon=folium.Icon(color='green',icon='info-sign'),
-    ).add_to(m)
-
-folium.Marker(
-    location= (coords[1],coords[0]),
-    tooltip='Вы тут',
-    popup='Вы тут',
-    icon=folium.Icon(icon='star'),
-    ).add_to(m)
-m.save('map.html')
-
-
-app = Flask(__name__)
-app.add_url_rule('/', 'hello', hello_world)
-app.run('0.0.0.0')
 if __name__ == '__main__':
-    main()
+    load_dotenv()
+    apikey = os.getenv('apikey')
+
+    with open('coffee.json', 'r', encoding='CP1251') as my_file:
+        cordinates = json.loads(my_file.read())
+
+    inputcord = input("Введите адрес: ")
+    coords = fetch_coordinates(apikey, inputcord)
+
+    cofe_spisok = []
+
+    for all_cofeina in cordinates:
+        all_cofeina_name = all_cofeina['Name']
+        all_cofeina_longitude = all_cofeina['Longitude_WGS84']
+        all_cofeina_latitude = all_cofeina['Latitude_WGS84']
+        all_cofeina_coordinates = [all_cofeina_longitude, all_cofeina_latitude]
+        all_cofeina_coordinates_two = [all_cofeina_latitude, all_cofeina_longitude]
+        func_distanse = distance.distance(coords, all_cofeina_coordinates).km
+        cofee = {
+            'Name': all_cofeina_name,
+            'Coordinates': all_cofeina_coordinates_two,
+            'Distanse': func_distanse
+        }
+        cofe_spisok.append(cofee)
+
+    min_distanse = sorted(cofe_spisok, key=get_func_distanse)
+    first_page = min_distanse[:5]
+    m = folium.Map(location=(coords[1], coords[0]), zoom_start=17)
+
+    for first_cofe_marker in first_page:
+        folium.Marker(
+            location=first_cofe_marker['Coordinates'],
+            tooltip='Нажми на меня!',
+            popup=first_cofe_marker['Name'],
+            icon=folium.Icon(color='green', icon='info-sign'),
+        ).add_to(m)
+
+    folium.Marker(
+        location=(coords[1], coords[0]),
+        tooltip='Вы тут',
+        popup='Вы тут',
+        icon=folium.Icon(icon='star'),
+    ).add_to(m)
+
+    m.save('map.html')
+
+
+    app = Flask(__name__)
+    app.add_url_rule('/', 'hello', hello_world)
+    app.run('0.0.0.0')
